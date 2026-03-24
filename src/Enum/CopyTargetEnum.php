60fd07e4b15ce21b91e123e099fa7ddf098f260a
Waldemar Schwab <4507670+schwabwaldemar@users.noreply.github.com>
2026-03-24 10:03:30 +0100

    Add more generic name

--- src/Enum/CopyTargetEnum.php
+++ src/Enum/CopyTargetEnum.php
@@ -9,7 +9,7 @@ enum CopyTargetEnum: string
     case PROJECT_ROOT = '.';
     case DDEV_COMMANDS_WEB = '.ddev/commands/web';
     case DDEV_COMMANDS_HOST = '.ddev/commands/host';
-    case PHPSTORM_EDITOR_CONFIG = '.idea';
+    case PHPSTORM_EDITOR_IDEA = '.idea';
     case PHPSTORM_RUN_CONFIG = '.run';
     case SYMFONY_BIN = 'bin';
     case SYMFONY_CONFIG_PACKAGES = 'config/packages';
@@ -22,7 +22,7 @@ enum CopyTargetEnum: string
     {
         return match ($this) {
             self::DDEV_COMMANDS_WEB, self::DDEV_COMMANDS_HOST                             => KnownSystemsEnum::DDEV,
-            self::PHPSTORM_RUN_CONFIG, self::PHPSTORM_EDITOR_CONFIG                       => KnownSystemsEnum::PHPSTORM,
+            self::PHPSTORM_RUN_CONFIG, self::PHPSTORM_EDITOR_IDEA                         => KnownSystemsEnum::PHPSTORM,
             self::SYMFONY_BIN, self::SYMFONY_CONFIG_PACKAGES, self::SYMFONY_CONFIG_ROUTES => KnownSystemsEnum::SYMFONY,
             self::GIT_HOOKS                                                               => KnownSystemsEnum::GIT,
             default                                                                       => null,
