ce9298c8d14057af721d0a2299d073c38a919ff0
Waldemar Schwab <4507670+schwabwaldemar@users.noreply.github.com>
2026-03-25 13:49:39 +0100

    feat: Add an option for .env.dev (#4)
    * Add an option for .env.dev
    
    * add idea to gitignore

--- src/Enum/EnvTargetEnum.php
+++ src/Enum/EnvTargetEnum.php
@@ -6,9 +6,10 @@ namespace Tbessenreither\Copycat\Enum;
 
 enum EnvTargetEnum: string
 {
+    case DOT_DEV = '.env.dev';
+    case DOT_EXAMPLE = '.env.example';
     case DOT_LOCAL = '.env.local';
     case DOT_TEST = '.env.test';
-    case DOT_EXAMPLE = '.env.example';
 
     public function getSystem(): ?KnownSystemsEnum
     {
